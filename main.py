dadd51312d705e0c7f584dbce8593039ed94f56d
Amogh R Shetty09 <amoghshetty193@gmail.com>
2026-03-09 19:58:46 +0530

    Remove comments for data cleaning

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ print("==========================================")
 from src.clean_data import clean_dataset
 from src.preprocess import preprocess_data
 
-# step 1
+
 clean_dataset()
 
-# step 2
+
 preprocess_data()
     
